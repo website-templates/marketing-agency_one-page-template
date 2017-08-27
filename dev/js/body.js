851b938ebaabcde8bf36f16a66caa732ded57435
Maxim Orlov <mail@orlovmax.com>
2017-08-28 02:29:56 +0300

    Add temaplates, styles and basic content

---
+++ dev/js/body.js
@@ -0,0 +1,9 @@
+// Include body scripts in the right order
+// They will be concatenated using gulp-rigger in body.min.js
+
+//= vendor/jquery.js
+//= lib/jquery.navigation.js
+//= lib/settings.js
+
+// Google map disabled by default because of empty API key
+// It is still available at lib/gmap.js
